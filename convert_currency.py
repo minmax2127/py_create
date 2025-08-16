31b45cc0afd6acbcf099b32ed12a8e644bdeaae0
minmax2127 <mbernales2127@gmail.com>
2025-08-16 18:28:37 +0800

    Add: Sample file

---
+++ convert_currency.py
@@ -0,0 +1,12 @@
+"""
+Currency Converter
+------------------
+Scrapes a currency converter website to get rates. (This is a sample of the file generated with the command.)
+"""
+
+def test_func():
+    """This is a sample description"""
+    print('Running test func...')
+
+if __name__ == '__main__':
+    test_func()
